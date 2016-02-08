243d01b1620c0ea67a6f0c10f89d590e2602c09f
Dan Roach <droach282@gmail.com>
2016-02-08 14:55:02 -0700

    Implemented Hand Ranking

--- kata-pokerhands.test/HandTests.cs
+++ kata-pokerhands.test/HandTests.cs
@@ -21,5 +21,20 @@ namespace kata_pokerhands.test
 			var hand = new Hand(input);
 			Assert.AreEqual(expected, hand.ToString());
 		}
+
+		[TestCase("TS 5H 3C 4D 6S", HandRank.HighCard)]
+		[TestCase("3D 3H 5C 4D 6S", HandRank.Pair)]
+		[TestCase("3H 4H 3S 4S 5D", HandRank.TwoPair)]
+		[TestCase("4H 4D 4C 6S 7S", HandRank.ThreeOfAKind)]
+		[TestCase("2H 3S 4D 5C 6S", HandRank.Straight)]
+		[TestCase("2H 4H 6H 8H TH", HandRank.Flush)]
+		[TestCase("2H 2C 2S 3H 3D", HandRank.FullHouse)]
+		[TestCase("2H 2C 2D 2S 3D", HandRank.FourOfAKind)]
+		[TestCase("2H 3H 4H 5H 6H", HandRank.StraightFlush)]
+		public void TestHandRanking(string input, HandRank expected)
+		{
+			var hand = new Hand(input);
+			Assert.AreEqual(expected, hand.Rank);
+		}
 	}
 }
--- kata-pokerhands/Hand.cs
+++ kata-pokerhands/Hand.cs
@@ -3,9 +3,25 @@ using System.Linq;
 
 namespace kata_pokerhands
 {
+	public enum HandRank
+	{
+		HighCard = 0,
+		Pair = 1,
+		TwoPair = 2,
+		ThreeOfAKind = 3,
+		Straight = 4,
+		Flush = 5,
+		FullHouse = 6,
+		FourOfAKind = 7,
+		StraightFlush = 8//,
+		//RoyalFlush = 9 // eh. Really this is a straight flush with a Ace high card.
+	}
+
 	public class Hand
 	{
 		private readonly Card[] _cards;
+
+		public HandRank Rank { get; private set; }
 
 		public Hand(string hand)
 		{
@@ -14,12 +30,57 @@ namespace kata_pokerhands
 				throw new ArgumentOutOfRangeException(nameof(cards), "hand must contain 5 cards");
 
 			_cards = cards.Select(x => new Card(x)).OrderBy(x => x).ToArray();
+
+			RankHand();
 		}
 
 		public override string ToString()
 		{
 			// ReSharper disable once CoVariantArrayConversion
 			return string.Join(" ", (object[])_cards);
+		}
+
+		private void RankHand()
+		{	
+			var isFlush = _cards.Select(x => x.Suit).Distinct().Count() == 1;
+
+			var distinctCards = _cards.Select(x => x.Value).Distinct().ToArray();
+			var isStraight = distinctCards.Count() == 5 && distinctCards.Max() - distinctCards.Min() == 4;
+
+			if (isStraight && isFlush)
+			{
+				Rank = HandRank.StraightFlush;
+			}
+			else if (isStraight)
+			{
+				Rank = HandRank.Straight;
+			}
+			else if (isFlush)
+			{
+				Rank = HandRank.Flush;
+			}
+			else if (distinctCards.Length < 5)
+			{
+				var groupedValues = _cards.GroupBy(x => x.Value).ToList();
+
+				switch (distinctCards.Length)
+				{
+					case 4:
+						Rank = HandRank.Pair;
+						break;
+					case 3:
+						Rank = groupedValues.Any(x => x.Count() == 3) ? HandRank.ThreeOfAKind : HandRank.TwoPair;
+						break;
+					case 2:
+						Rank = groupedValues.Any(x => x.Count() == 4) ? HandRank.FourOfAKind : HandRank.FullHouse;
+						break;
+				}
+			}
+			else
+			{
+				// High card (sad trombone)
+				Rank = HandRank.HighCard;
+			}
 		}
 	}
 }
